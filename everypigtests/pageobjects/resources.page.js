d4578f3676bb08bf33e1d1aabf09fa45a3581b91
Vladyslav Reva <juve1991@gmail.com>
2020-01-15 10:34:49 +0200

    Add mobile view support for resources.spec

--- everypigtests/pageobjects/resources.page.js
+++ everypigtests/pageobjects/resources.page.js
@@ -2,26 +2,45 @@
 var ReportPage = require('./report.page');
 
 class ResourcesPage extends ReportPage {
-    get url() { return this.baseUrl + '/tenant-assets'; }
-
-    get input() {
-        return $$('input[type=text]');
+    constructor() {
+        super();
+        this.row = '[class^=asset-item-row]';
     }
-    get assetBox() { return $$('div[class^=asset-item-box]'); }
+
+    get url() { return this.baseUrl + '/tenant-assets'; }
+    get input() { return $$('input[type=text]'); }
+    get assetBox() { return $$('[class^=asset-item-box]'); }
     get tenantRes() { return $('.TenantResources'); }
-    get uploadList() { return $('div[class^="uploads-list_"]'); }
+    get uploadList() { return $('[class^=uploads-list_]'); }
+
+    //tableRow is changed to avoid header of table
+    get tableRow() { return '.table-row:not(.show-for-large)'; }
+
+    assetName(idx = 0) {
+        return isMobile
+            ? $$(`${this.row} [class^=name]`)[idx]
+            : this.cell('',0, idx).$('[class^=asset-name]');
+    }
+
+    get assetType() { return this.cell('', 1); }
+    get addWrapper() { return isMobile ? '.StickyFooter' : '[class^=resources-sub-nav]'; }
 
     clickAddLink() { return $('.add-resource-square=Link').waitClick() && this.waitLoader(); }
     clickGrigView() { return $('i.fa.fa-grid-two-up').waitClick() && this.waitLoader(); }
     clickListView() { return $('i.fa.fa-ulist').waitClick() && this.waitLoader(); }
 
-    setName(str) {
-        return this.input[0].waitSetValue(str) && this;
+    clickBtn(str, wrapper) {
+        wrapper = (str === 'Add') ? this.addWrapper : wrapper;
+        return super.clickBtn(str, wrapper);
     }
 
-    setUrl(str) {
-        return this.input[1].waitSetValue(str) && this;
+    clickMenuCell(str, row = 0) {
+        const wrap = $$(this.row)[row];
+        return isMobile ? this.clickDots(wrap) : super.clickMenuCell(str, row);
     }
+
+    setName(str) { return this.input[0].waitSetValue(str) && this; }
+    setUrl(str) { return this.input[1].waitSetValue(str) && this; }
 
     waitUpload() { return this.uploadList.waitForExist(undefined, true) && this; }
 }
--- everypigtests/specs/resources.spec.js
+++ everypigtests/specs/resources.spec.js
@@ -4,176 +4,184 @@ describe('Resources', () => {
     beforeEach(function () {
         this.currentTest.retries(1);
         this.currentTest._currentRetry > 0 && resPage.reload();
+        //because Sorting functionality is not available in mobile view
+        isMobile && this.currentTest.title.includes('Sort') && this.skip();
     });
 
     it('Open', () => {
-        resPage.clickSidebar('Resources');
+        isMobile ? resPage.clickDots('.main-footer').clickMobileMenu('Resources')
+            : resPage.clickSidebar('Resources');
 
         expect(browser.getUrl(), 'url').match(/\/tenant-assets/);
     });
 
     it('Add link', () => {
         const link = 'www.example.com', name = tdata.randComment;
-        resPage.clickBtn('Add').clickAddLink()
-            .setName(name).setUrl(link).clickToModal('Create');
+        resPage.clickBtn('Add')
+            .clickAddLink()
+            .setName(name)
+            .setUrl(link)
+            .clickToModal('Create');
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(name);
-        expect(resPage.cell('',1,1).getText(), 'type').to.equal('Weblink');
+        expect(resPage.assetName().getText(), 'name').to.equal(name);
+        isMobile || expect(resPage.assetType.getText(), 'type').to.equal('Weblink');
     });
 
     it('Edit link', () => {
         const link = 'example.com', name = tdata.randComment;
-        resPage.clickMenuCell('', 1).clickOption('Edit Link')
-            .setName(name).setUrl(link).clickToModal('Save');
+        resPage.clickMenuCell()
+            .clickOption('Edit Link')
+            .setName(name)
+            .setUrl(link)
+            .clickToModal('Save');
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(name);
-        expect(resPage.cell('',1,1).getText(), 'type').to.equal('Weblink');
+        expect(resPage.assetName().getText(), 'name').to.equal(name);
+        isMobile || expect(resPage.assetType.getText(), 'type').to.equal('Weblink');
     });
 
     it('Add video', () => {
-        const video = tdata.randVideo;
-        resPage.clickBtn('Add').uploadMedia(video).waitUpload();
-        //browser.pause(3000);
+        const name = tdata.randVideo;
+        resPage.clickBtn('Add')
+            .uploadMedia(name).waitUpload().pause(1500);
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(video);
-        expect(resPage.cell('',1,1).getText(), 'type').to.equal('Video');
+        expect(resPage.assetName().getText(), 'name').to.equal(name);
+        isMobile || expect(resPage.assetType.getText(), 'type').to.equal('Video');
     });
 
     it('Download video', () => {
-        resPage.clickMenuCell('', 1).clickOption('Download');
-        const file = resPage.cell('',0,1).getText();
+        resPage.clickMenuCell()
+            .clickOption('Download');
+        const file = resPage.assetName().getText();
 
         resPage.checkFileExists(file, 30000);
     });
 
     it('Edit video name', () => {
-        const name = resPage.cell('',0,1).getText() + '_edited';
-        resPage.clickMenuCell('', 1).clickOption('Edit Name')
-            .setName(name).clickToModal('Save');
+        const name = resPage.assetName().getText() + '_edited';
+        resPage.clickMenuCell()
+            .clickOption('Edit Name')
+            .setName(name)
+            .clickToModal('Save');
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(name);
-        expect(resPage.cell('',1,1).getText(), 'type').to.equal('Video');
+        expect(resPage.assetName().getText(), 'name').to.equal(name);
+        isMobile || expect(resPage.assetType.getText(), 'type').to.equal('Video');
     });
 
     it('Open video', () => {
-        resPage.cell('',0,1).$('div[class^="asset-name"]').waitClick();
-        resPage.waitLoader();
+        resPage.clickOn(resPage.assetName());
 
         expect(resPage.mediaViewer.$('video').isExisting(), 'mediaViewer').to.equal(true);
 
-        browser.pause(1500);
-        resPage.mediaViewer.doubleClick();
-        browser.pause(1500);
-        resPage.mediaViewer.doubleClick();
-
+        resPage.pause(1500).mediaViewer.doubleClick();
+        resPage.pause(1500).mediaViewer.doubleClick();
         resPage.clickCloseView();
 
         expect(resPage.mediaViewer.isExisting(), 'mediaViewer').to.equal(false);
     });
 
-
     it('Add photo', () => {
-        const photo = tdata.randPhoto;
-        resPage.clickBtn('Add').uploadMedia(photo).waitUpload();
-        browser.pause(1500);
+        const name = tdata.randPhoto;
+        resPage.clickBtn('Add')
+            .uploadMedia(name).waitUpload().pause(1500);
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(photo);
-        expect(resPage.cell('',1,1).getText(), 'type').to.equal('Image');
+        expect(resPage.assetName().getText(), 'name').to.equal(name);
+        isMobile || expect(resPage.assetType.getText(), 'type').to.equal('Image');
     });
 
     it('Download photo', () => {
-        resPage.clickMenuCell('', 1).clickOption('Download');
-        const file = resPage.cell('',0,1).getText();
+        resPage.clickMenuCell()
+            .clickOption('Download');
+        const file = resPage.assetName().getText();
 
         resPage.checkFileExists(file, 30000);
     });
 
     it('Edit photo name', () => {
-        const name = resPage.cell('',0,1).getText() + '_edited';
-        resPage.clickMenuCell('', 1).clickOption('Edit Name')
-            .setName(name).clickToModal('Save');
+        const name = resPage.assetName().getText() + '_edited';
+        resPage.clickMenuCell()
+            .clickOption('Edit Name')
+            .setName(name)
+            .clickToModal('Save');
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(name);
-        expect(resPage.cell('',1,1).getText(), 'type').to.equal('Image');
+        expect(resPage.assetName().getText(), 'name').to.equal(name);
+        isMobile || expect(resPage.assetType.getText(), 'type').to.equal('Image');
     });
 
     it('Open photo', () => {
-        resPage.cell('',0,1).$('div[class^="asset-name"]').waitClick();
-        resPage.waitLoader();
+        resPage.clickOn(resPage.assetName()).pause(1500);
 
         expect(resPage.mediaViewer.$('img').isExisting(), 'mediaViewer').to.equal(true);
 
-        browser.pause(1500);
-        resPage.clickScalePlus().clickScalePlus()
+        isMobile || resPage.clickScalePlus().clickScalePlus()
             .clickScaleOrig().clickScaleMinus().clickScaleMinus();
-
         resPage.clickCloseView();
 
         expect(resPage.mediaViewer.isExisting(), 'mediaViewer').to.equal(false);
     });
 
     it('Add document', () => {
-        const doc = tdata.randDoc;
-        resPage.clickBtn('Add').uploadMedia(doc).waitUpload();
-        browser.pause(1500);
+        const name = tdata.randDoc;
+        resPage.clickBtn('Add')
+            .uploadMedia(name).waitUpload().pause(1500);
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(doc);
-        expect(resPage.cell('',1,1).getText(), 'type').to.equal('DOC');
+        expect(resPage.assetName().getText(), 'name').to.equal(name);
+        isMobile || expect(resPage.assetType.getText(), 'type').to.equal('DOC');
     });
 
     it('Download document', () => {
-        resPage.cell('',0,1).$('div[class^="asset-name"]').waitClick();
-        const file = resPage.cell('',0,1).getText();
+        resPage.clickOn(resPage.assetName()).pause(1500);
+        const file = resPage.assetName().getText();
 
         resPage.checkFileExists(file, 30000);
     });
 
     it('Edit document name', () => {
-        const name = resPage.cell('',0,1).getText() + '_edited';
-        resPage.clickMenuCell('', 1).clickOption('Edit Name')
-            .setName(name).clickToModal('Save');
+        const name = resPage.assetName().getText() + '_edited';
+        resPage.clickMenuCell()
+            .clickOption('Edit Name')
+            .setName(name)
+            .clickToModal('Save');
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(name);
-        expect(resPage.cell('',1,1).getText(), 'type').to.equal('DOC');
+        expect(resPage.assetName().getText(), 'name').to.equal(name);
+        isMobile || expect(resPage.assetType.getText(), 'type').to.equal('DOC');
     });
 
     it('Add pdf', () => {
-        const pdf = tdata.randPdf;
-        resPage.clickBtn('Add').uploadMedia(pdf).waitUpload();
-        browser.pause(1500);
+        const name = tdata.randPdf;
+        resPage.clickBtn('Add')
+            .uploadMedia(name).waitUpload().pause(1500);
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(pdf);
-        expect(resPage.cell('',1,1).getText(), 'type').to.equal('PDF');
+        expect(resPage.assetName().getText(), 'name').to.equal(name);
+        isMobile || expect(resPage.assetType.getText(), 'type').to.equal('PDF');
     });
 
     it('Download pdf', () => {
-        resPage.clickMenuCell('', 1).clickOption('Download');
-        const file = resPage.cell('',0,1).getText();
+        resPage.clickMenuCell()
+            .clickOption('Download');
+        const file = resPage.assetName().getText();
 
         resPage.checkFileExists(file, 30000);
     });
 
     it('Edit pdf name', () => {
-        const name = resPage.cell('',0,1).getText() + '_edited';
-        resPage.clickMenuCell('', 1).clickOption('Edit Name')
+        const name = resPage.assetName().getText() + '_edited';
+        resPage.clickMenuCell().clickOption('Edit Name')
             .setName(name).clickToModal('Save');
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(name);
-        expect(resPage.cell('',1,1).getText(), 'type').to.equal('PDF');
+        expect(resPage.assetName().getText(), 'name').to.equal(name);
+        isMobile || expect(resPage.assetType.getText(), 'type').to.equal('PDF');
     });
 
     it('Open pdf', () => {
-        resPage.cell('',0,1).$('div[class^="asset-name"]').waitClick();
-        resPage.waitLoader();
+        resPage.clickOn(resPage.assetName()).pause(1500);
 
         expect(resPage.mediaViewer.$('.react-pdf__Document').isExisting(), 'mediaViewer').to.equal(true);
 
-        resPage.clickScalePlus().clickScalePlus().clickScaleOrig()
+        isMobile || resPage.clickScalePlus().clickScalePlus().clickScaleOrig()
             .clickScaleMinus().clickScaleMinus();
-        //browser.pause(1500);
 
-        $('div[data-page-number="2"]').scrollIntoView({block: "center"});
-        $('div[data-page-number="3"]').scrollIntoView({block: "center"});
+        $('[data-page-number="2"]').scrollIntoView({block: "center"});
+        $('[data-page-number="3"]').scrollIntoView({block: "center"});
 
         resPage.clickCloseView();
 
@@ -182,11 +190,12 @@ describe('Resources', () => {
 
     it('Add unsupported file format', () => {
         const file = tdata.randAudio;
-        resPage.clickBtn('Add').uploadMedia(file);
+        resPage.clickBtn('Add')
+            .uploadMedia(file);
 
         expect(resPage.notification.getText(), 'error text').to.have
             .string(file + ' has type not allowed, please upload files of type');
-        expect(resPage.cell('',0,1).getText(), 'name').to.not.equal(file);
+        expect(resPage.assetName().getText(), 'name').to.not.equal(file);
     });
 
     it('Grid view', () => {
@@ -204,16 +213,16 @@ describe('Resources', () => {
     it('Sort by name', () => {
         resPage.clickSortBy('Name');
 
-        let idx = --resPage.tableRowsWith('').length,
-            first = resPage.cell('',0,1).getText(),
-            last = resPage.cell('',0,idx).getText();
+        let idx = --resPage.tableRowsWith().length,
+            first = resPage.assetName().getText(),
+            last = resPage.assetName(idx).getText();
 
         expect(first <= last, `${first} <= ${last}`).to.equal(true);
 
         resPage.clickSortBy('Name');
 
-        first = resPage.cell('',0,1).getText();
-        last = resPage.cell('',0,idx).getText();
+        first = resPage.assetName().getText();
+        last = resPage.assetName(idx).getText();
 
         expect(first >= last, `${first} >= ${last}`).to.equal(true);
     });
@@ -221,16 +230,16 @@ describe('Resources', () => {
     it('Sort by type', () => {
         resPage.clickSortBy('Type');
 
-        let idx = --resPage.tableRowsWith('').length,
-            first = resPage.cell('',1,1).getText(),
-            last = resPage.cell('',1,idx).getText();
+        let idx = --resPage.tableRowsWith().length,
+            first = resPage.cell('',1).getText(),
+            last = resPage.cell('',1, idx).getText();
 
         expect(first <= last, `${first} <= ${last}`).to.equal(true);
 
         resPage.clickSortBy('Type');
 
-        first = resPage.cell('',1,1).getText();
-        last = resPage.cell('',1,idx).getText();
+        first = resPage.cell('',1).getText();
+        last = resPage.cell('',1, idx).getText();
 
         expect(first >= last, `${first} >= ${last}`).to.equal(true);
     });
@@ -238,27 +247,27 @@ describe('Resources', () => {
     it('Sort by date', () => {
         resPage.clickSortBy('Date');
 
-        let idx = --resPage.tableRowsWith('').length,
-            first = resPage.cell('',2,1).getText(),
-            last = resPage.cell('',2,idx).getText();
+        let idx = --resPage.tableRowsWith().length,
+            first = resPage.cell('',2).getText(),
+            last = resPage.cell('',2, idx).getText();
 
         expect(first <= last, `${first} <= ${last}`).to.equal(true);
 
         resPage.clickSortBy('Date');
 
-        first = resPage.cell('',2,1).getText();
-        last = resPage.cell('',2,idx).getText();
+        first = resPage.cell('',2).getText();
+        last = resPage.cell('',2, idx).getText();
 
         expect(first >= last, `${first} >= ${last}`).to.equal(true);
     });
 
     it('Search', () => {
-        let idx = --resPage.tableRowsWith('').length,
-            last = resPage.cell('',0,idx).getText();
+        let idx = --(isMobile ? $$(resPage.row) : resPage.tableRowsWith()).length,
+            last = resPage.assetName(idx).getText();
 
         resPage.setSearch(last);
 
-        expect(resPage.cell('',0,1).getText(), 'name').to.equal(last);
+        expect(resPage.assetName().getText(), 'name').to.equal(last);
     });
 
     it('Search special chars', () => {
@@ -272,11 +281,10 @@ describe('Resources', () => {
 
     it('Remove', () => {
         resPage.reload();
-        const rows = resPage.tableRowsWith('').length,
-            idx = tdata.rand(rows - 2) + 1;
-        file = resPage.cell('', 0, idx).getText();
+        let idx = --(isMobile ? $$(resPage.row) : resPage.tableRowsWith()).length,
+            file = resPage.assetName(idx).getText();
         resPage.clickMenuCell('', idx).clickOption('Remove');
-        const fileAfter = resPage.cell('', 0, idx).getText();
+        const fileAfter = resPage.assetName(idx - 1).getText();
 
         expect(resPage.notification.getText(), 'error text')
             .to.equal('Asset has been deleted!');
